359a9bfd5f9fe323f176590c9ec4fc2aba1899c7
bweedop <bweedop@aggiemail.usu.edu>
2017-06-22 12:07:07 -0600

    jun22

--- dropping_tip.R
+++ dropping_tip.R
@@ -2,7 +2,7 @@ library(caper)
 library(geiger)
 
 
-drop_random_tips<-function(spp)
+drop_random_tips<-function(spp, dropped_fraction)
 {
   tree<-sim.bdtree(n = spp)
   write.tree(tree, file = "original_random_tree.tre")
@@ -13,10 +13,12 @@ drop_random_tips<-function(spp)
   write.table(ed, file = "original_random_tree_data.txt")
   
   imputed_tree<-tree
-  for (i in seq_len(spp/10))
+  for (i in seq_len(spp*dropped_fraction))
   {
-    spp_to_be_dropped<-sample(imputed_tree$tip.label)
-    imputed_tree<-drop.tip(imputed_tree, i)
+    spp_to_be_dropped<-sample(imputed_tree$tip.label, 1)
+    delete_row<-as.numeric(which(ed$species == spp_to_be_dropped))
+    ed<-ed[-delete_row,]
+    imputed_tree<-drop.tip(imputed_tree, spp_to_be_dropped)
   }
   write.tree(imputed_tree, file = "imputed_random_tree.tre")
   
@@ -24,23 +26,27 @@ drop_random_tips<-function(spp)
   imputed_ed<-imputed_ed[order(imputed_ed$ED, decreasing = TRUE),]
   rownames(imputed_ed)<-1:nrow(imputed_ed)
   write.table(imputed_ed, file = "imputed_random_tree_data.txt")
+  
+  ed_corr<-cor(ed[2], imputed_ed[2])
+  return(ed_corr)
 }
 
 
-drop_clustered_tips<-function(spp)
+drop_clustered_tips<-function(spp, dropped_fraction)
 {
   tree<-sim.bdtree(n = spp)
   write.tree(tree, file = "original_clustered_tree.tre")
   
   ed<-ed.calc(tree)$spp
-  ed<-ed[order(ed$ED, decreasing = TRUE),]
-  rownames(ed)<-1:nrow(ed)
-  write.table(ed, file = "original_clustered_tree_data.txt")
+  ordered_ed<-ed[order(ed$ED, decreasing = TRUE),]
+  rownames(ordered_ed)<-1:nrow(ordered_ed)
+  write.table(ordered_ed, file = "original_clustered_tree_data.txt")
   
   imputed_tree<-tree
   brown_evol<-sim.char(tree, 0.05, 1, model = "BM")[,,1]
-  quantile <- quantile(brown_evol, 0.9)
+  quantile <- quantile(brown_evol, 1-0.1)
   to.drop <- which(brown_evol >= quantile)
+  ed<-ed[-to.drop,]
   imputed_tree <- drop.tip(tree, to.drop)
   write.tree(imputed_tree, file = "imputed_clustered_tree.tre")
   
@@ -48,11 +54,29 @@ drop_clustered_tips<-function(spp)
   imputed_ed<-imputed_ed[order(imputed_ed$ED, decreasing = TRUE),]
   rownames(imputed_ed)<-1:nrow(imputed_ed)
   write.table(imputed_ed, file = "imputed_clustered_tree_data.txt")
+  
+  ed_corr<-cor(ed[2], imputed_ed[2])
+  return(ed_corr)
 }
 
+data_wrapper<-function(method)
+{
+  data <- expand.grid(n.spp=c(200,400,800,1000,1200), fraction.dropped=c(.01,.05,.1,.2), edge.corr=NA)
+  if (method == "clustered")
+  {
+    for(i in seq_len(nrow(data)))
+    {
+      data$edge.corr[i] <- drop_clustered_tips(data$n.spp[i], data$fraction.dropped[i])
+    }
+  }else if(method == "random"){
+    for(i in seq_len(nrow(data)))
+    {
+      data$edge.corr[i] <- drop_random_tips(data$n.spp[i], data$fraction.dropped[i])
+    }
+  }else{
+    print("Invalid method entry")
+  }
 
-data <- expand.grid(n.spp=c(50,100,500,1000), fraction.dropped=c(.01,.05,.1,.2), edge.corr=NA)
-
-for(i in seq_len(nrow(data)))
-    data$edge.corr <- with(data, n.spp[i] * fraction.dropped[i]) #...or something more useful...
+  return(data)
+}
 
