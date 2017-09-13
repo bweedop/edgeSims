32c08b4528a156feac9aedc31db974f114744d20
bweedop <bodie.weedop@aggiemail.usu.edu>
2017-09-13 16:37:30 -0600

    ready for the server

--- clade_manip.R
+++ clade_manip.R
@@ -89,8 +89,61 @@ wrapper<-function(n.spp=seq(600, 1200, by = 200), clade.size=seq(2, 10,by=1)){
 }
 
 
-get.ed <- function(spp, size)
+get.full.ed <- function(spp, size)
 {
+  #Running the function in order to get the ED value correlations for the full phylogenies.
+  size.met <- FALSE
+  while (!size.met)
+  {
+    tree <- sim.bdtree(n = spp)
+    clade.mat <- clade.matrix(tree)$clade.matrix
+    mat.sums <- rowSums(clade.mat)
+    size.met <- any(mat.sums == size)
+  }
+  selected.clades <- which(mat.sums == size)
+  if (length(selected.clades) == 1){
+    clade.number <- selected.clades
+  }else{
+    clade.number<-sample(selected.clades, 1)
+  }
+  random.clade <- extract.clade(tree, clade.number)
+  focal.clade.spp <- unique(na.omit(as.numeric(unlist(strsplit(unlist(random.clade$tip.label), "[^0-9]+")))))
+  #Getting the spp of the focal clade so that the ED values of these spp can be selected out of 
+  #original.ed and imputed.ed.
+  original.ed<-ed.calc(tree)$spp
+  original.ed <- setNames(original.ed[,2], original.ed[,1])
+  node.name <- which(names(branching.times(tree)) == clade.number)
+  node.age <- branching.times(tree)[node.name]
+  nasty.bl <- dist.nodes(tree)[clade.number,][clade.number-1]
+  gammatree<-ltt(tree, plot = FALSE, gamma = FALSE)
+  gamma <- gammatest(gammatree)$gamma
+  
+  dropped.tree<-drop.tip(tree, sample(random.clade$tip.label, length(random.clade$tip.label)-1))
+  
+  r<-NULL
+  while(is.null(r)){
+    try({
+      donor.clade<-sim.bdtree(n = size)
+      donor.clade$tip.label <- random.clade$tip.label#letters[seq_along(donor.clade$tip.label)]
+      for (i in dropped.tree$tip.label){
+        if(any(random.clade$tip.label==i)){
+          tip<-i
+        }
+      }
+      r<-bind.replace(dropped.tree, donor.clade, tip)
+      dropped.tree<-r})
+  }
+  imputed.ed<-ed.calc(dropped.tree)$spp
+  imputed.ed <- setNames(imputed.ed[,2], imputed.ed[,1])
+  full.ed.corr <- cor(original.ed[tree$tip.label], imputed.ed[tree$tip.label])
+  focal.ed.corr<-cor(original.ed[focal.clade.spp], imputed.ed[focal.clade.spp])
+  return(full.ed.corr)
+}
+
+get.focal.ed <- function(spp, size)
+{
+  #Running the function in order to get the ED value correlations for the focal clades.
+  
   size.met <- FALSE
   while (!size.met)
   {
@@ -140,21 +193,33 @@ get.ed <- function(spp, size)
 }
 
 
-multiple.wrapper<-function(n.spp= c(64, 128, 256, 512, 1024), clade.size=c(4, 8, 16, 32), reps = 10){
+multiple.wrapper<-function(n.spp= c(64, 128, 256, 512, 1024), clade.size=c(2, 3, 4, 8, 16, 32), reps = 100){
+  
   params <- expand.grid(n.spp=n.spp, clade.size=clade.size)
-  data <- matrix(nrow=nrow(params), ncol=reps)
+  
+  full.data <- matrix(nrow=nrow(params), ncol=reps)
   for(i in seq_len(nrow(params))){
-    for (j in seq_len(ncol(data))){
-      data[i,j] <- get.ed(params$n.spp[i], params$clade.size[i])
+    for (j in seq_len(ncol(full.data))){
+      full.data[i,j] <- get.full.ed(params$n.spp[i], params$clade.size[i])
     }
   }
-  data <- cbind(params, data)
-  colnames(data)<-c("n.spp","clade.size","trial.1","trial.2", "trial.3", "trial.4", "trial.5",
-                    "trial.6", "trial.7", "trial.8", "trial.9", "trial.10")
   
-  graph<-ggplot(data, aes(clade.size, edge.corr, colour=factor(n.spp))) + geom_line()
-  graph+labs(x = "Size of Dropped Clade", y = "Correlation of ED Values", colour = "No. of Total Spp")
-  return(data)
+  focal.data <- matrix(nrow=nrow(params), ncol=reps)
+  for(i in seq_len(nrow(params))){
+    for (j in seq_len(ncol(focal.data))){
+      focal.data[i,j] <- get.focal.ed(params$n.spp[i], params$clade.size[i])
+    }
+  }
+  
+  
+  colnames(full.data)<-paste("trial.", 1:reps, sep = "")
+  colnames(focal.data)<-paste("trial.", 1:reps, sep = "")
+  
+  full.data <- cbind(params, full.data)
+  focal.data <- cbind(params, focal.data)
+  
+  write.table(full.data, file = "FullCorrelationData.txt")
+  write.table(focal.data, file = "FocalCorrelationData.txt")
 }
 
 
--- dropping_tip.R
+++ dropping_tip.R
@@ -6,29 +6,35 @@ library(ggplot2)
 
 drop_random_tips<-function(spp, dropped_fraction)
 {
+  #Simulate phylogeny and write phylogeny data to file.
   tree<-sim.bdtree(n = spp)
   write.tree(tree, file = "original_random_tree.tre")
   
+  #Calculate ED values for the original tree and record to .txt file.
   ed<-ed.calc(tree)$spp
   ed<-ed[order(ed$ED, decreasing = TRUE),]
   rownames(ed)<-1:nrow(ed)
   write.table(ed, file = "original_random_tree_data.txt")
   
+  #Use the original tree for the tree which will have a determined fraction of random tips dropped from it. 
   imputed_tree<-tree
   for (i in seq_len(spp*dropped_fraction))
   {
-    spp_to_be_dropped<-sample(imputed_tree$tip.label, 1)
+    spp_to_be_dropped<-sample(imputed_tree$tip.label, 1) 
     delete_row<-as.numeric(which(ed$species == spp_to_be_dropped))
     ed<-ed[-delete_row,]
     imputed_tree<-drop.tip(imputed_tree, spp_to_be_dropped)
   }
+  #Write the manipulated tree to a file for documentation.
   write.tree(imputed_tree, file = "imputed_random_tree.tre")
   
+  #Calculate ED values for the manipulated tree and write them to a .txt file for documentation.
   imputed_ed<-ed.calc(imputed_tree)$spp
   imputed_ed<-imputed_ed[order(imputed_ed$ED, decreasing = TRUE),]
   rownames(imputed_ed)<-1:nrow(imputed_ed)
   write.table(imputed_ed, file = "imputed_random_tree_data.txt")
   
+  #Calculate the correlation between the ED values of the original tree and the manipulated tree.
   ed_corr<-cor(ed[2], imputed_ed[2])
   return(ed_corr)
 }
@@ -36,16 +42,22 @@ drop_random_tips<-function(spp, dropped_fraction)
 
 drop_clustered_tips<-function(spp, dropped_fraction)
 {
+  #Simulate phylogeny and write phylogeny data to file.
   tree<-sim.bdtree(n = spp)
   write.tree(tree, file = "original_clustered_tree.tre")
   
+  #Calculate ED values for the original tree and record to .txt file.
   ed<-ed.calc(tree)$spp
   ordered_ed<-ed[order(ed$ED, decreasing = TRUE),]
   rownames(ordered_ed)<-1:nrow(ordered_ed)
   write.table(ordered_ed, file = "original_clustered_tree_data.txt")
   
+  #Use the original tree for the tree which will have a determined fraction of clustered tips dropped from it.
   imputed_tree<-tree
+  
+  #Simulate continuous trait data on the original tree using a constant rate Brownian-motion model.
   brown_evol<-sim.char(tree, 0.05, 1, model = "BM")[,,1]
+  #Assessing which spp fall into the percentile which is to be dropped and the dropping them from the tree.
   quantile <- quantile(brown_evol, 1-dropped_fraction)
   to.drop <- which(brown_evol >= quantile)
   if (dropped_fraction != 0){
@@ -56,18 +68,22 @@ drop_clustered_tips<-function(spp, dropped_fraction)
   }
   write.tree(imputed_tree, file = "imputed_clustered_tree.tre")
   
+  #Calculate the ED values for the manipulated tree and record the data.
   imputed_ed<-ed.calc(imputed_tree)$spp
   ordered_imputed_ed<-imputed_ed[order(imputed_ed$ED, decreasing = TRUE),]
   rownames(ordered_imputed_ed)<-1:nrow(ordered_imputed_ed)
   write.table(ordered_imputed_ed, file = "imputed_clustered_tree_data.txt")
   
+  #Calculate the correlation between the original ED values and the manipulated trees' ED values.
   ed_corr<-cor(ed[2], imputed_ed[2])
   return(ed_corr)
 }
 
 data_wrapper<-function(method, reps)
 {
-  data <- expand.grid(n.spp=seq(600,1200,by=200), fraction.dropped=seq(0,0.2,by=0.01), edge.corr=0)
+  #Wrapper that can run either of the methods (random or clustered) and then returns the data and graphs for the
+  # amount of repititions set by the user.
+  data <- expand.grid(n.spp = c(64, 128, 256, 512, 1024), fraction.dropped = seq(0,0.2,by=0.01), edge.corr=0)
   if (method == "clustered")
   {
     for (i in seq_len(reps))
@@ -88,6 +104,7 @@ data_wrapper<-function(method, reps)
   }else{
     print("Invalid method entry")
   }
+  
   data[,3]<-data[,3]/reps
   graph<-ggplot(data, aes(fraction.dropped, edge.corr, colour=factor(n.spp))) + geom_line()
   graph+labs(x = "Fraction Dropped", y = "Correlation of ED", colour = "No. of Spp")
